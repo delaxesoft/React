b0d0f86a041f847a744cc58d9c95f319c357fa27
delaxesoft <delaxesoft@gmail.com>
2022-09-29 06:48:48 +0100

    First Commit

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Main() {
+  return (
+<main>
+  <h1 className='main--title'>Fun Facts about React</h1>
+  <ul className='main--facts'>
+    <li>It was first developed in 2003</li>
+    <li>Was Originally created by Jordan Walker</li>
+    <li>Has well over 100k Stars</li>
+    <li>Is maintained by facebook</li>
+
+  </ul>
+  <footer>
+    <small>@ 2022 Delaxesoft Development</small>
+  </footer>
+</main>  )
+}
+
+
+export default Main
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav >
+       <img src="./images/react-icon.png" className='nav--icon'/>
+       <h3 className='nav--logo-text'> React Journey</h3>
+       <h4 className='nav-title'>Swimming in React</h4>
+    </nav>
+  )
+}
+
+export default Navbar
